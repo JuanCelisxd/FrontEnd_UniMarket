36991c8d8b46b2c1d4c24df7b92ebbde56fcbbd0
JuanCelis1 <juancarlosc04@gmail.com>
2023-04-28 14:57:52 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './pagina/inicio/inicio.component';
-import { LoginComponent } from './pagina/login/login.component';
-import { RegistroComponent } from './pagina/registro/registro.component';
+import { InicioComponent } from './pagina/inicio/inicio.component.ts';
+import { LoginComponent } from './pagina/login/login.component.ts';
+import { RegistroComponent } from './pagina/registro/registro.component.ts';
 
 const routes: Routes = [
   {path : "", component: InicioComponent},
