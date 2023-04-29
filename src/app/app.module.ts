a4f9f3cdcdbd15c38065028097a250e9be501dbc
JuanCelisxd <juancarlosc04@gmail.com>
2023-04-28 21:39:18 -0500

    Registro y crear producto funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './Pagina/inicio/inicio.component';
 import { LoginComponent } from './Pagina/login/login.component';
 import { RegistroComponent } from './Pagina/registro/registro.component';
+import { CrearProductoComponent } from './Pagina/crear-producto/crear-producto.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    CrearProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
