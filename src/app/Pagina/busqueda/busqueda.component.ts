4b853f247d92ea3e82779e13759567e1d85e8de6
JuanCelis1 <juancarlosc04@gmail.com>
2023-05-28 03:05:35 -0500

    vista admin moderador creada

--- src/app/Pagina/busqueda/busqueda.component.ts
+++ src/app/Pagina/busqueda/busqueda.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { ProductoGetDTO } from 'src/app/modelo/producto-get-dto';
+import { ProductoModeradorDto } from 'src/app/modelo/producto-moderador-dto';
+import { ProductoEstadosService } from 'src/app/servicios/producto-estados.service';
 import { ProductoService } from 'src/app/servicios/producto.service';
 
 @Component({
@@ -14,17 +16,21 @@ export class BusquedaComponent {
 
   textoBusqueda: string;
   productos: ProductoGetDTO[];
+  productosAprobados:ProductoModeradorDto[];
   filtro: ProductoGetDTO[];
 
-  constructor(private route:ActivatedRoute, private router:Router, private productoServicio:ProductoService){
+  constructor(private route:ActivatedRoute, private router:Router, private productoServicio:ProductoService, private productoAp:ProductoEstadosService){
     this.textoBusqueda = "";
     this.productos = this.productoServicio.listar();
+    this.productosAprobados = this.productoAp.listar();
     this.filtro = [];
     this.route.params.subscribe(params=> {
       this.textoBusqueda = params['texto'];
       this.filtro = this.productos.filter(p => 
       p.nombre.toLocaleLowerCase().includes(this.textoBusqueda.toLocaleLowerCase()));
       console.log(this.textoBusqueda);
+      this.filtro = this.productosAprobados.filter(p =>
+        p.nombre.toLocaleLowerCase().includes(this.textoBusqueda.toLocaleLowerCase()));
     })
   }
   public iraBusqueda(valor: string) {
